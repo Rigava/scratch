20f4f45288cc85468e3d4ca4e51b5415b7215d92
Rigava <josharun1@gmail.com>
2026-07-26 15:12:17 +0530

    userLogins

--- momentum_screener/screener/admin.py
+++ momentum_screener/screener/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import UserProfile, TradeJournal
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_username', 'get_email', 'trial_started_at', 'total_allowed_days', 'days_remaining', 'is_active', 'is_premium')
+    search_fields = ('user__username', 'user__email')
+    list_filter = ('is_premium',)
+
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = 'Username'
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
+
+    def is_active(self, obj):
+        return obj.is_trial_active()
+    is_active.boolean = True
+    is_active.short_description = 'Trial Active?'
+
+@admin.register(TradeJournal)
+class TradeJournalAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ticker', 'trade_type', 'status', 'entry_date', 'entry_price', 'exit_date', 'exit_price', 'pnl')
+    search_fields = ('user__username', 'ticker')
+    list_filter = ('status', 'trade_type')
+
